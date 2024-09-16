6702e0dd6c6a4b7734a620d246cda1a5dee31656
Alexander Polanco <alexander.polanco@gmail.com>
2024-09-16 17:15:32 -0300

    Tutorial completed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://e6e44178.astro-blog-tutorial-6co.pages.dev/",
+  integrations: [preact()],
+});
